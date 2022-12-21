d59a845c59e5a1ebac3cd3c11fa49856ecb3df0f
doganoguz <info@doganoguz.com>
2022-12-21 18:46:08 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+// @dart=2.9
+
+//bu bir Doğan Oğuz projesidir.
+
+//"Eğer beni güldürmek istiyorsan, beni react native ile tehdit edeceksin aslanım"
+
+import 'package:askingucu/ui/page/dashboard/dashboard.dart';
+import 'package:askingucu/ui/page/onboard/onboard.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter/services.dart';
+
+Future<void> main() async {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent, //statusbar color değiştirme
+  ));
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Aşkın Gücü',
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/onboard',
+        routes: <String, WidgetBuilder>{
+          '/onboard': (BuildContext context) => new Onboard(),
+          '/dashboard': (BuildContext context) => new Dashboard(),
+        });
+  }
+}
---
+++ lib/ui/page/onboard/onboard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Onboard extends StatefulWidget {
+  const Onboard({super.key});
+
+  @override
+  State<Onboard> createState() => _OnboardState();
+}
+
+class _OnboardState extends State<Onboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
