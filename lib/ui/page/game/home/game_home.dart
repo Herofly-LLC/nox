7d9dd01693bef2b14fb0bc4ac9234e6bfaa23fbc
doganoguz <info@doganoguz.com>
2023-01-05 12:04:59 +0300

    game home added

---
+++ lib/ui/page/game/home/game_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GameHome extends StatefulWidget {
+  const GameHome({super.key});
+
+  @override
+  State<GameHome> createState() => _GameHomeState();
+}
+
+class _GameHomeState extends State<GameHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
